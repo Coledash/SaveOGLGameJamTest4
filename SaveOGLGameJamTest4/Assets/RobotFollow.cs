0c1b4ac1bbfa2c13f2faeb9f97d943fd8218ccd1
Coledash <viskipullo@gmail.com>
2019-08-02 16:31:35 +0300

    Robot

---
+++ SaveOGLGameJamTest4/Assets/RobotFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotFollow : MonoBehaviour
+{
+    public Transform target;
+    Vector3 distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        distance = transform.position - target.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = target.position + distance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ SaveOGLGameJamTest4/Assets/RobotMain.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotMain : MonoBehaviour
+{
+    Rigidbody rb;
+    public float moveSpeed;
+    public float rotationSpeed;
+    public Transform top;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit, 100))
+        {
+            Vector3 viewDirection = hit.point - transform.position;
+            viewDirection.y = 0;
+            top.rotation = Quaternion.LookRotation(viewDirection);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 movement = transform.forward * vertical * moveSpeed * Time.deltaTime;
+        rb.MovePosition(transform.position + movement);
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        Quaternion rotation = Quaternion.Euler(0, horizontal * rotationSpeed * Time.deltaTime, 0);
+        rb.MoveRotation(rb.rotation * rotation);
+    }
+}
